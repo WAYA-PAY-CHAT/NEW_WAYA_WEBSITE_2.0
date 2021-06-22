41904ed2d167863c26c46ddff3fb113ab0204d19
Ai-Bee <iboroinyang01@gmail.com>
2021-06-22 12:55:24 +0100

    Added social media share buttons

--- src/pages/single-post.js
+++ src/pages/single-post.js
@@ -5,7 +5,7 @@ import PageFooter from '../componentParts/footer'
 import TopNav from '../componentParts/topNav'
 import { useParams } from "react-router-dom";
 
-function SinglePost(){
+function SinglePost(props){
     let { postId } = useParams();
     const [post, setPost] = useState({})
     let dateFormater = (theDate) => {
@@ -14,8 +14,13 @@ function SinglePost(){
         return dateobj.toLocaleDateString(undefined, options)
     }
 
+    let formatPostTitle = (text) => {
+        let pieces = text.split(' ')
+        let joined = pieces.join('%20')
+        return joined
+    }
+
     useEffect(() => {
-        console.log(postId)
         axios.get(`https://waya-pay-chat.herokuapp.com/posts/1`).then(res => {
             console.log(res)
             setPost(res.data)
@@ -41,6 +46,18 @@ function SinglePost(){
                         {post.content}
                     </section>
                     
+                    </div>
+                    <div className='row ml-3 mb-4'>
+                        <div className='col-sm-2'>
+                        <a href={`https://twitter.com/intent/tweet?url=https://wayachat-preview.netlify.app/blog/${postId}`}>
+                        <img src='/twitter.png' style={{maxWidth: '20%'}} alt='twitter'/>
+                        </a>
+                        </div>
+                        <div className='col-sm-2'>
+                        <a href={`https://www.facebook.com/sharer/sharer.php?u=https://wayachat-preview.netlify.app/blog/${postId}`}>
+                        <img src='/facebook.png' style={{maxWidth: '10%'}}  alt='facebook'/>
+                        </a>
+                        </div>
                     </div>
                     </div>
                 </div>
