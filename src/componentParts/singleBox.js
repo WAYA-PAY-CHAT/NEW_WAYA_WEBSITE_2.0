cd96f2d0da5771afee79ee96df2f53d36061061a
Iboro <33674453+Ai-Bee@users.noreply.github.com>
2021-07-21 01:15:46 +0100

    Update singleBox.js

--- src/componentParts/singleBox.js
+++ src/componentParts/singleBox.js
@@ -3,6 +3,7 @@ function SingleBox(props){
     return (     
         <div 
         className='singleBox' 
+        id={props.givenId}
         style={{backgroundColor: `${props.bgColor}`}}>
             <img src={props.imageName}/>
             <h6>{props.title}</h6>
