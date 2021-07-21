4eb7a0ea7943efae753c33754a9d30abfb6e6cba
Iboro <33674453+Ai-Bee@users.noreply.github.com>
2021-07-21 01:22:15 +0100

    Update wayagram.js

--- src/pages/wayagram.js
+++ src/pages/wayagram.js
@@ -11,41 +11,49 @@ function WayaGram(){
         {
             title: 'make post',
             image: '/post.png',
+            id: 'post',
             text: 'Make short posts and share across moments with friends and families'
         },
         {
             title: 'join group',
             image: '/Group.png',
+            id:'group',
             text: 'Join group, follow events, like and follow personal and business pages'
         },
         {
             title: 'Socialize',
             image: '/socializeIcon.png',
+            id:'socialize',
             text: 'Socialise with the power of a growing community on WayaGram, express yourself within the limits of boundless freedom'
         },
         {
             title: 'Vote',
             image: '/vote.png',
+            id:'vote',
             text: 'Participate in poll on WayaGram, vote for friends and families with WayaGram'
         },
         {
             title: 'enjoy moments',
             image: '/smileys.png',
+            id: 'moments',
             text: 'Watch short clips of your followers or any other WayaGram user. enjoy momemts, like and share with others in your timeline'
         },
         {
             title: 'advertise',
+            id: 'advertise',
             image: '/marketing.png',
             text: 'Advertise and sell your product on the fastest growing social media platfrom growing from Nigeria and spreading throughout Africa'
         },
         {
             title: 'share live screen',
             image: '/live.png',
+            id: 'live',
             text: 'Share live screen with anyone to follow and watch you on the move and as you host events. distance is not a barrier with the live screen feature of WayaGram. Share precious moments as they are happening live.'
         },
         {
             title: 'campaign',
             image: '/coms.png',
+            id: 'campaign',
             text: 'Launch campaigns, anyone or entity can launch campaigns on WayaGram for personal, corporate, public and non-governmental events. WayaGram is the perfect tool for social momets'
         }
     ])
@@ -71,6 +79,7 @@ function WayaGram(){
                 return <SingleBox bgColor={'white'}
                 title={el.title}
                 body={el.text}
+                givenId={el.id}
                  imageName={el.image}/>
            })}           
            </div>
